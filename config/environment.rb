840d8c21b817efd2fbd8d6f0d6eada2f9c70f8df
Mik <jacknidle@gmail.com>
2013-09-20 15:08:46 +0400

    fully functional application, concert scanner task working

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/protected.yml")
 # Initialize the rails application
 MusicObserver::Application.initialize!
